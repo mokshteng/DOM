c41480b97142fd8621cbc6cf74db3e338683bc7a
mokshteng <moksh.teng@byjus.com>
2023-02-06 22:00:43 +0530

    bugs fixed

--- Q2/index.js
+++ Q2/index.js
@@ -1,11 +1,11 @@
 const button = document.getElementById("btn");
-
+const card=document.getElementById("container")
 const fetchData = async () => {
-  button.style.opacity = "0.5";
+  
   let name = document.getElementById("name");
   let email = document.getElementById("email");
   let phone = document.getElementById("phone");
-  const id=Math.floor(Math.random() * 10) + 1;
+  const id = Math.floor(Math.random() * 10) + 1;
   const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
 
   let person = await data.text();
@@ -14,7 +14,11 @@ const fetchData = async () => {
   name.textContent = `Name: ${person.name}`;
   email.textContent = `Email: ${person.email}`;
   phone.textContent = `Phone: ${person.phone}`;
-  button.style.opacity = "1";
-  document.getElementById("info").textContent=JSON.stringify(person);
+  document.getElementById("info").textContent = JSON.stringify(person);
 };
-button.addEventListener("click", fetchData);
+const changeColor = (event) =>{
+  button.style.backgroundColor='red';
+  event.stopPropagation();
+}
+button.addEventListener("click",changeColor,false)
+card.addEventListener("click", fetchData);
